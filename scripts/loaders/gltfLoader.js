c556dc4daacc420c7f8d89e99ff024fce573df1d
Zyko <micaiahejardin@gmail.com>
2026-04-01 17:54:51 -0500

    Delete scripts directory

--- scripts/loaders/gltfLoader.js
+++
@@ -1,8 +0,0 @@
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
-
-export function loadGLTF(url) {
-  return new Promise((resolve, reject) => {
-    const loader = new GLTFLoader();
-    loader.load(url, resolve, undefined, reject);
-  });
-}
